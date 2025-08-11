e55f4483ec4041cf219b82561e0e8e89bb1dfc81
Yuriy Mishchenko <y1mishchenko@gmail.com>
2025-08-11 19:53:21 +0200

    Resolve posts with multiple images

--- scripts/tumblr.py
+++ scripts/tumblr.py
@@ -1,4 +1,5 @@
 import os
+import re
 from pathlib import Path
 
 import requests
@@ -7,8 +8,6 @@ from file_metadata import FileMetadata, FileMetadataHelper
 from helper import Helper
 from requests_oauthlib import OAuth1
 
-# TODO: find method to get oauth_token
-# TODO: Set the configuration to determine the art resolution, default: max
 # TODO: look into type, date, post_url attibutes of the post
 # TODO: look into https://www.tumblr.com/docs/en/api/v2#postsdraft--retrieve-draft-posts
 # TODO: look into https://www.tumblr.com/docs/en/api/v2#postsqueue--retrieve-queued-posts
@@ -30,6 +29,7 @@ class TumblrCollector:
             resource_owner_key=os.getenv("TUMBLR_OAUTH_TOKEN"),
             resource_owner_secret=os.getenv("TUMBLR_OAUTH_SECRET"),
         )
+        self.url_pattern = re.compile(r"\s*(https?://[^\s]+)\s+([0-9]+)w\s*")
 
     def get_followed_blogs(self) -> list[str]:
         resp = requests.get(
@@ -45,35 +45,54 @@ class TumblrCollector:
         is_first_run = len(previous_tumblr_blogs) == 0
 
         for blog_name in current_blog_names:
-            current_blog_files = 0
-            params = {}
-            is_new_blog = blog_name not in previous_tumblr_blogs
-
-            if is_first_run or is_new_blog:
-                # Don't use 'after' parameter for the first run or new blogs
-                pass
-            else:
-                last_runtime = self.helper.get_last_runtime_in_unix()
-                params["after"] = last_runtime
-
-            resp = requests.get(
-                f"https://api.tumblr.com/v2/blog/{blog_name}.tumblr.com/posts",
-                auth=self.oauth,
-                params=params,
-                timeout=10,
+            blog_files = self._get_files_from_blog(
+                blog_name, is_first_run, previous_tumblr_blogs
             )
-            posts = resp.json()["response"]["posts"]
-
-            for post in posts:
-                file = self.file_meta.populate_file_metadata(
-                    raw_content=post["trail"][0]["content_raw"], author=blog_name
-                )
-
-                if file:
-                    files.append(file)
-                    current_blog_files += 1
-
-                if current_blog_files == self.FILE_LIMIT_PER_BLOG:
-                    break
+            files.extend(blog_files)
 
         return files
+
+    def _get_files_from_blog(
+        self, blog_name: str, is_first_run: bool, previous_tumblr_blogs: list[str]
+    ) -> list[FileMetadata]:
+        blog_files: list[FileMetadata] = []
+        params = {}
+        is_new_blog = blog_name not in previous_tumblr_blogs
+
+        if is_first_run or is_new_blog:
+            # Don't use 'after' parameter for the first run or new blogs
+            pass
+        else:
+            last_runtime = self.helper.get_last_runtime_in_unix()
+            params["after"] = last_runtime
+
+        resp = requests.get(
+            f"https://api.tumblr.com/v2/blog/{blog_name}.tumblr.com/posts",
+            auth=self.oauth,
+            params=params,
+            timeout=10,
+        )
+        posts = resp.json()["response"]["posts"]
+
+        for post in posts:
+            content_raw = post["trail"][0]["content_raw"]
+
+            # Split content_raw as a post can have multiple images
+            srcset_matches = re.findall(r'srcset="([^"]+)"', content_raw)
+            if srcset_matches:
+                for srcset_content in srcset_matches:
+                    file_candidates = srcset_content.split(",")
+                    # Get the file with the highest resolution
+                    last_candidate_url = file_candidates[-1].split()[0]
+
+                    file = self.file_meta.populate_file_metadata(
+                        url=last_candidate_url, author=blog_name
+                    )
+
+                    if file:
+                        blog_files.append(file)
+
+                    if len(blog_files) == self.FILE_LIMIT_PER_BLOG:
+                        return blog_files
+
+        return blog_files
